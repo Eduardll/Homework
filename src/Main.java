e037aeb9487b70eda6d4a935949ae712d74d472d
Eduardll <fediks45@gmail.com>
2025-04-11 14:51:09 +0300

    kekes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        MyHashMap<Integer, String> gg = new MyHashMap<>();
+        gg.put(12,"sd");
+        gg.put(13,"dd");
+        gg.put(14,"fd");
+        gg.put(15,"gd");
+        gg.get(14);
+        System.out.println(gg.isEmpty());
+        gg.display();
+        gg.clear();
+        gg.display();
     }
 }
